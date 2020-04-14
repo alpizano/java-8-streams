0526234a72a83d5052b055126f1f9fcb774ae170
al <apizano@purdue.edu>
2020-04-13 21:46:44 -0500

    adding example 2

---
+++ src/ImperativeVsDeclarativeExample2.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ImperativeVsDeclarativeExample2 {
+    
+    public static void main(String[] main) {
+        List<Integer> integerList = Arrays.asList(1,2,3,4,5,5,6,7,8,8,9,10);
+    }
+}
