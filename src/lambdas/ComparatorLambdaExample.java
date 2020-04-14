77f641e515a54ab183788485beb138877b2fde3f
al <apizano@purdue.edu>
2020-04-13 23:52:12 -0500

    Implementing comparator interface with Lambda

--- src/lambdas/ComparatorLambdaExample.java
+++ src/lambdas/ComparatorLambdaExample.java
@@ -15,11 +15,21 @@ public class ComparatorLambdaExample {
                 return o1.compareTo(o2); //  0 -> o1 == o2
                                          //  1 -> o1 > o2
                                          // -1 -> o1 < o2
-                
+
             }
         };
 
         System.out.println("Result of the Comparator is: " + comparator.compare(3,2));
 
+        // Lambda can be replaced with method reference
+        Comparator<Integer> comparatorLambda = (Integer a, Integer b) -> a.compareTo(b);
+
+        System.out.println("Result of the Comparator using Lambda: " + comparatorLambda.compare(3,2));
+
+        // Lambda can be replaced with method reference. Do not need to mention Integer type since Integer type is already provided
+        // Lambda knows to typecast input to Integer
+        Comparator<Integer> comparatorLambda1 = (a, b) -> a.compareTo(b);
+
+        System.out.println("Result of the Comparator using Lambda: " + comparatorLambda1.compare(3,2));
     }
 }
