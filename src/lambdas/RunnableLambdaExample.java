8b1a573c31ee2bfd7100940a4c38190b06650d44
al <apizano@purdue.edu>
2020-04-13 22:13:46 -0500

    implementing Runnable pre-Java8

--- src/lambdas/RunnableLambdaExample.java
+++ src/lambdas/RunnableLambdaExample.java
@@ -1,4 +1,22 @@
 package lambdas;
 
 public class RunnableLambdaExample {
+    public static void main(String[] args) {
+        /**
+         * Prior to Java 8
+         */
+
+        // Create anonymous class
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Inside Runnable 1");
+            }
+        };
+
+        // Pass runnable to Thread and run method
+        new Thread(runnable).start();
+
+
+    }
 }
