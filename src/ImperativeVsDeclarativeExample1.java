9ed12f67f98fabb15accbabdfcc7712af8f1e7fe
al <apizano@purdue.edu>
2020-04-13 21:03:52 -0500

    Added imperative approach summation of nums of to 100

--- src/ImperativeVsDeclarativeExample1.java
+++ src/ImperativeVsDeclarativeExample1.java
@@ -1,2 +1,15 @@
 public class ImperativeVsDeclarativeExample1 {
+    public static void main(String[] args) {
+        /**
+         * Imperative - how style of programming
+         */
+
+        int sum = 0;
+        for(int i=0; i<=100; i++) {
+            sum+=i;
+        }
+
+        System.out.println("Sum using Imperative Approach: " + sum);
+    }
+
 }
