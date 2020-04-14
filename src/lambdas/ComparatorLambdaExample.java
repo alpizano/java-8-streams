70ac8baa1fb163ab6d34cf91ab1335c19f99fd15
al <apizano@purdue.edu>
2020-04-13 22:34:28 -0500

    Implementing comparator pre Java 8

--- src/lambdas/ComparatorLambdaExample.java
+++ src/lambdas/ComparatorLambdaExample.java
@@ -1,7 +1,25 @@
 package lambdas;
 
+import java.util.Comparator;
+
 public class ComparatorLambdaExample {
     public static void main(String[] args) {
-        
+
+        /**
+         * Prior to Java 8
+         */
+
+        Comparator<Integer> comparator = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2); //  0 -> o1 == o2
+                                         //  1 -> o1 > o2
+                                         // -1 -> o1 < o2
+                
+            }
+        };
+
+        System.out.println("Result of the Comparator is: " + comparator.compare(3,2));
+
     }
 }
